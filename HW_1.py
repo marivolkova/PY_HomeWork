beb9270323f91594865f7d4d2dbccaa22e700e66
Marina <mari7723@inbox.ru>
2023-03-21 20:25:46 +0300

    1

--- HW_1.py
+++ HW_1.py
@@ -80,3 +80,18 @@
 #     print('YES!')
 # else:
 #     print('NO')
+
+
+n = int(input("Введите целое число "))
+count_zero = 0
+count_one = 0
+for i in range(n):
+    x = int(input())
+    if x == 0:
+        count_zero += 1
+    else:
+        count_one += 1
+if count_one > count_zero:
+    print(count_zero)
+else:
+    print(count_one)
